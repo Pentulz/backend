0f59111123f5d35e21d78e59abf97a46ff5bd1ca
NATSIIRT <tristan.baud@proton.me>
2025-08-19 16:20:33 +0200

    chore: add pylint and adapt workflow

--- tests/test_health.py
+++ tests/test_health.py
@@ -7,14 +7,4 @@ def test_health_ok():
     r = client.get("/api/v1/health")
     assert r.status_code == 200
     assert r.json()["status"] == "ok"
-
-# For future tests, if we have async endpoints : 
-
-"""
-@pytest.mark.asyncio
-async def test_health_ok_async():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        r = await ac.get("/api/v1/health")
-    assert r.status_code == 200
-    assert r.json()["status"] == "ok"
-"""
+    
