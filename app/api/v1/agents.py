288820ee5325893b8a5f3acb3330f32b1de7f06b
Thynkon <thynkon@protonmail.com>
2025-08-31 13:58:35 +0200

    fix(agent): return agents name in http responses

--- app/api/v1/agents.py
+++ app/api/v1/agents.py
@@ -33,6 +33,7 @@ async def get_agents(db: AsyncSession = Depends(get_db)):
         response.append(
             Agent(
                 id=agent.id,
+                name=agent.name,
                 hostname=agent.hostname,
                 description=agent.description,
                 platform=agent.platform,
@@ -77,6 +78,7 @@ async def update_agent(
 
         updated_agent = Agent(
             id=agent_from_db.id,
+            name=agent_from_db.name,
             hostname=agent_from_db.hostname,
             description=agent_from_db.description,
             platform=agent_from_db.platform,
@@ -113,6 +115,7 @@ async def get_agent(agent_id: str, db: AsyncSession = Depends(get_db)):
 
     response = Agent(
         id=agent.id,
+        name=agent.name,
         hostname=agent.hostname,
         description=agent.description,
         platform=agent.platform,
@@ -152,6 +155,7 @@ async def create_agent(agent: AgentCreate, db: AsyncSession = Depends(get_db)):
 
         response = Agent(
             id=new_agent.id,
+            name=new_agent.name,
             hostname=new_agent.hostname,
             description=new_agent.description,
             platform=new_agent.platform,
